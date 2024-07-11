467a87b448620280d198cd7bbc90ee4cc7a6cf04
Kaoru Nishikawa <kq.nishikawa@gmail.com>
2024-07-12 03:12:31 +0900

    Add basic buttons for user interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { type ReactNode, useState } from 'react'
+import { type ReactNode, useRef, useState } from 'react'
+import Button from '../components/Button'
 import styles from './page.module.scss'
 import './globals.scss'
 
 export default function Home(): ReactNode {
     const [element, setElement] = useState<HTMLElement | null>(null)
+    const ref = useRef<HTMLDivElement>(null)
 
     const handleInput = (event: React.ChangeEvent<HTMLDivElement>) => {
         // Clone the element, otherwise mutation of element won't trigger re-rendering
@@ -13,12 +15,32 @@ export default function Home(): ReactNode {
         setElement(element)
     }
 
+    const handleCopy = () => {
+        if (!element) return
+        navigator.clipboard.writeText(element?.innerHTML)
+    }
+
+    const handleClear = () => {
+        setElement(null)
+        if (ref.current) {
+            ref.current.innerHTML = ''
+        }
+    }
+
     return (
         <main className={styles.main}>
-            <div className={styles.panel} contentEditable={true} onInput={handleInput}>
+            <div className={styles.panel}>
+                <div
+                    className={styles.textField}
+                    contentEditable={true}
+                    onInput={handleInput}
+                    ref={ref}
+                />
+                <Button onClick={handleClear}>Clear</Button>
             </div>
             <div className={styles.panel}>
-                {element?.innerHTML}
+                <div className={styles.textField}>{element?.innerHTML}</div>
+                <Button onClick={handleCopy}>Copy</Button>
             </div>
         </main>
     )
